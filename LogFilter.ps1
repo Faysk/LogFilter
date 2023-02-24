653df922264d2e6b88d4303482a5c509bd4e3ec3
Renan Silva <faysk.nan@gmail.com>
2023-02-24 08:26:49 -0300

    Upload v1

---
+++ LogFilter.ps1
@@ -0,0 +1,31 @@
+# Testa a conexão com o servidor Server-Name e armazena o resultado na variável $testConnectionServer
+$testConnectionServer = Test-Connection Server-Name -Quiet
+
+# Define o caminho do diretório de log em $logPath
+$logPath = "\\Server-Name\log\*"
+
+# Entra em um loop que continuará enquanto o servidor Server-Name estiver disponível
+while ($testConnectionServer) {
+    # Conta o número de arquivos de log para cada nível (INFO, ERROR, WARNING) usando Select-String
+    $infoCount = (Get-ChildItem -Path $logPath -Filter *.log | Select-String ": INFO").Count
+    $errorCount = (Get-ChildItem -Path $logPath -Filter *.log | Select-String ": ERROR").Count
+    $warningCount = (Get-ChildItem -Path $logPath -Filter *.log | Select-String ": WARNING").Count
+
+    # Conta o número total de linhas em todos os arquivos de log usando Get-Content, Measure-Object e a propriedade Sum
+    $totalCount = (Get-ChildItem -Path $logPath -Filter *.log | Get-Content | Measure-Object -Line).Sum
+
+    # Cria uma string contendo as informações de contagem de logs, incluindo a contagem total de linhas
+    $logInfo = "Quantidade de informativos: $infoCount`nQuantidade de erros: $errorCount`nQuantidade de avisos: $warningCount`nQuantidade total: $totalCount"
+
+    # Exibe as informações de contagem de logs, incluindo a contagem total de linhas
+    Write-Output $logInfo
+    Write-Output ""
+
+    # Exibe as últimas 20 linhas dos arquivos de log
+    Write-Output "20 últimas linhas de log: "
+    Get-Content -Path $logPath -Filter *.log | Select-Object -Last 20
+
+    # Aguarda 60 segundos antes de limpar o console e começar o loop novamente
+    Start-Sleep -Seconds 60
+    Clear-Host
+}
